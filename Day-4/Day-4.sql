e15204516f3b7e59b7da3ad5288df2286af6c6e5
Apurva <apurvapatil71@gmail.com>
2026-01-09 15:15:16 +0100

    Add files via upload

---
+++ Day-4/Day-4.sql
@@ -0,0 +1,30 @@
+/*1. Display the first 5 patients from the patients table.
+2. Show patients 11-20 using OFFSET.
+3. Get the 10 most recent patient admissions based on arrival_date.*/
+
+SELECT TOP 5 *
+FROM patients;
+
+SELECT *
+FROM patients
+ORDER BY patient_id
+OFFSET 10 ROWS
+FETCH NEXT 10 ROWS ONLY;
+
+SELECT TOP 10 *
+FROM patients
+ORDER BY arrival_date DESC;
+
+/*Question: Find the 3rd to 7th highest patient satisfaction scores from the patients table, 
+showing patient_id, name, service, and satisfaction. Display only these 5 records.*/
+
+
+SELECT patient_id,
+	name,
+	service,
+	satisfaction
+FROM patients
+ORDER BY satisfaction DESC
+OFFSET 2 ROWS
+FETCH NEXT 5 ROWS ONLY;
+
